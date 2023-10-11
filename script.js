7b7474cd50a7125985fcc67d41df8420b1934434
Nikita Dubinin <nik.dubinin.01@mail.ru>
2023-10-11 21:19:59 +0300

    удалил рекламму

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 const books = document.querySelectorAll(".book");
 const body = document.querySelector("body");
 let boldTitle = document.querySelectorAll(".book>h2>a");
+const removeAdv = document.querySelector(".adv");
 
 console.log(boldTitle);
 
@@ -13,5 +14,7 @@ books[1].after(books[0]);
 body.classList.add("new-image");
 
 boldTitle[4].innerHTML = "Книга 3. this и <strong>Прототипы</strong> Объектов";
+
+removeAdv.remove();
 
 console.log(books);
