63dbe1c583d24bc76ba70015f0c8cc9d906dcd2f
Nikita Dubinin <nik.dubinin.01@mail.ru>
2023-10-11 23:12:41 +0300

    добавил элемент li в шестую книгу, поставил в нужное место

--- script.js
+++ script.js
@@ -2,10 +2,33 @@
 
 const books = document.querySelectorAll(".book");
 const body = document.querySelector("body");
+const allLi = document.getElementsByTagName("li");
 let boldTitle = document.querySelectorAll(".book>h2>a");
 const removeAdv = document.querySelector(".adv");
 const chaptersOfTheSecondBook = books[0].querySelectorAll(".book>ul>li");
 const chaptersOfTheFifthBook = books[5].querySelectorAll(".book>ul>li");
+const chaptersOfTheSixthBook = books[2].querySelectorAll("li");
+
+let chaptersOfTheSixthBookById;
+let chaptersOfTheSixthBookByClass;
+
+let EightChapters = function () {
+  for (let i = 0; i <= chaptersOfTheSixthBook.length - 1; i++) {
+    chaptersOfTheSixthBook[i].id = "li";
+    chaptersOfTheSixthBook[i].classList.add("li");
+  }
+
+  chaptersOfTheSixthBookById = document.getElementById("li");
+
+  chaptersOfTheSixthBookById.insertAdjacentHTML(
+    "beforebegin",
+    "<li id=li> <strong> Глава 8: За пределами ES6 </strong> </li>"
+  );
+
+  chaptersOfTheSixthBookByClass = books[2].querySelectorAll(".book>ul>li");
+
+  chaptersOfTheSixthBookByClass[9].after(chaptersOfTheSixthBookByClass[0]);
+};
 
 let sortingBooks = function () {
   books[5].after(books[2]);
@@ -46,10 +69,7 @@ let start = function () {
   removeAdvFunction();
   sortingTheSecondBook();
   sortingTheFifthBook();
+  EightChapters();
 };
 
 start();
-
-console.log(chaptersOfTheFifthBook);
-
-console.log(books);
