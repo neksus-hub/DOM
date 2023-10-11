ffb62b9e79eed50dfa36934d76703cd8238c95a4
Nikita Dubinin <nik.dubinin.01@mail.ru>
2023-10-11 21:03:42 +0300

    изменил background

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 "use strict";
 
 const books = document.querySelectorAll(".book");
+const body = document.querySelector("body");
+
+console.log(body);
 
 books[5].after(books[2]);
 books[5].before(books[3]);
 books[1].after(books[0]);
+
+body.classList.add("new-image");
 
 console.log(books);
