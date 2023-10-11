2fd58afea6ec77268fd99046647f7dffa370f5ac
Nikita Dubinin <nik.dubinin.01@mail.ru>
2023-10-11 21:17:30 +0300

    изменил заголовок

--- script.js
+++ script.js
@@ -2,13 +2,16 @@
 
 const books = document.querySelectorAll(".book");
 const body = document.querySelector("body");
+let boldTitle = document.querySelectorAll(".book>h2>a");
 
-console.log(body);
+console.log(boldTitle);
 
 books[5].after(books[2]);
 books[5].before(books[3]);
 books[1].after(books[0]);
 
 body.classList.add("new-image");
+
+boldTitle[4].innerHTML = "Книга 3. this и <strong>Прототипы</strong> Объектов";
 
 console.log(books);
